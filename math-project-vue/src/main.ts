88bafbfb420e727246698dcf4dbf205af742f218
dq1234678 <3551139554@qq.com>
2025-09-16 15:48:12 +0800

    xiugai

--- math-project-vue/src/main.ts
+++ math-project-vue/src/main.ts
@@ -5,3 +5,9 @@ import './style.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+import i18nPlugin from "./plugins/i18n";
+
+const app = createApp(App);
+app.use(i18nPlugin);
+app.mount("#app");
